f00736cfbb48133da36e265ec42cf17c0584f9d6
Usuario <Usuario@DESKTOP-3UAOJIF>
2023-11-09 08:32:20 -0300

    Merge origin/master
    Conflicts:
    	src/conexion/MySql.java

--- paginas/planillas/imprimirSalario.php
+++ paginas/planillas/imprimirSalario.php
@@ -65,13 +65,71 @@ if ($query->rowCount()) {
     </style>
     <head>
         <meta charset="UTF-8">
-        <title>DETALLE INDIVUAL DE SALARIO DEL MES</title>
+        
         <script src="../../css/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
         <link href="../../css/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">    </head>
     <body style="margin: 30px auto; width: 700px;
           box-shadow: 1px 1px 10px 5px #999999; padding: 50px;">
-        <img src="../../images/membrete.jpg" style="width: 100%;" alt="">
-        <h2 style="margin-top: 20px;"><center>DETALLE INDIVIDUAL DE SALARIO DEL MES</center></h2>
+        <img src="../../images/originalcabecera.png" style="width: 100%;" alt="">
+        
+        <hr> 
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <h3>Datos del empleado</h3>
+                </div>
+                <div class="col-md-3">
+                    <label>Nombre y Apellido: </label><br> <?php echo $arreglo[0]['personal'] ?>
+
+                </div>
+                <div class="col-md-3">
+                    <label>Cédula: </label><br><?php echo $arreglo[0]['cedula'] ?>
+
+                </div>
+                <div class="col-md-3">
+                    <label>Cargo: </label><br><?php echo $arreglo[0]['car_descri'] ?>
+
+                </div>
+                <div class="col-md-3">
+                    <label>Estado: </label><br><?php echo $arreglo[0]['estado'] ?>
+
+                </div>
+
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12">
+
+                    <h3>Liquidación de salario</h3>
+                </div>
+                <div class="col-md-12">
+                    <label>Salario base </label><br> <?php echo number_format(is_numeric($arreglo[0]['sal_id']) ? $arreglo[0]['sal_id'] : 0, 0, ',', '.') ?>
+
+                </div>
+                <div class="col-md-12">
+                    <label>Total Extras </label><br> <?php echo number_format(is_numeric($arreglo[0]['total_extra']) ? $arreglo[0]['total_extra'] : 0, 0, ',', '.') ?>
+
+                </div>
+                <div class="col-md-12">
+                    <label>Total Descuentos </label><br> <?php echo number_format(is_numeric($arreglo[0]['total_descuento']) ? $arreglo[0]['total_descuento'] : 0, 0, ',', '.') ?>
+
+                </div>
+                <div class="col-md-12">
+                    <label>Salario Neto </label><br> <?php echo number_format(is_numeric($arreglo[0]['sal_mes']) ? $arreglo[0]['sal_mes'] : 0, 0, ',', '.') ?>
+
+                </div>
+                
+                
+               
+            </div>
+        </div>
+
+    </body>
+    <body style="margin: 30px auto; width: 700px;
+          box-shadow: 1px 1px 10px 5px #999999; padding: 50px;">
+        <img src="../../images/copiacabecera.png" style="width: 100%;" alt="">
+        
         <hr> 
         <div class="container">
             <div class="row">
